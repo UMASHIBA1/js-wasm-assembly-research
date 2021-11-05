b64a18b97ffc67508daec08338593f75ace0512c
UMASHIBA <umashiba1@gmail.com>
2021-11-05 18:03:44 +0900

    add: create rust fib

---
+++ fib/rust/js/index.js
@@ -0,0 +1,3 @@
+import("../pkg/index.js").then(module => {
+    module.fib(45);
+});
---
+++ fib/rust/src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn fib(n: i64) -> i64 {
+    if n <= 1 {
+        return n
+    }
+
+    fib(n - 1) + fib(n - 2)
+}
